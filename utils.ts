225866d0cddf38e94542d6a60e9a738a2befca31
applearon <aronseshan@gmail.com>
2024-03-11 20:49:21 -0400

    Update utils

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 import { parseTypes } from './types.ts'
-export async function returnChatMsg(string, playerID, players) {
+export async function returnChatMsg(string, playerID) {
     let pid = 0x0d;
     return parseTypes([pid, playerID, string], ['hex', 'hex', 'string']);
 }

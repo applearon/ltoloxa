ac04647ff4561f9acb7d101d296330734f1e1829
applearon <aronseshan@gmail.com>
2024-03-12 00:19:20 -0400

    Add buildWorld function

--- examples/simple.ts
+++ examples/simple.ts
@@ -1,46 +1,24 @@
 #!/usr/bin/env bun
 import { ClientPacket, CPlayerID, CSetBlock, CMsg, PlayerPos, Player, parseShort, parseString, parseTypes } from '../types.ts';
-import { getID, returnServerID, sendWorld,spawnPlayer } from '../loginHelpers.ts';
+import { getID, returnServerID, sendWorld, spawnPlayer } from '../loginHelpers.ts';
 import { broadcast, parseClientData } from '../socketHelpers.ts';
-import { returnChatMsg } from '../utils.ts';
+import { returnChatMsg, buildWorld, placeBlock } from '../utils.ts';
 import { lto } from '../index.ts';
 //const WorldSize = [512, 64, 512];
 //let players = new Map();
 let spawnPos = {x: 256, y: 40, z: 256, yaw: 0x00, pitch: 0x00} as PlayerPos;
 let World = { x: 512, y: 64, z: 512, players: new Map(), deltas: []} as World;
-let deltas = [];
 let server;
-async function buildWorld(World, deltas) {
-    let lx = World.x;
-    let lz = World.z;
-    let ly = World.y;
-    let size = lx * ly * lz;
-    let hexarr = size.toString(16).padStart(8, "0").match(/\d{2}/g);
-    const buffer = Buffer.alloc(size);
-    for (let i = 0; i < hexarr.length; i++) {
-        hexarr[i] = parseInt(hexarr[i],16);
-        buffer[i] = hexarr[i];
-        //writer.write(buffer);
+const getBlock = (World, x, y, z) => {
+    let block = 0x00;
+    if (y < World.y/2) {
+        block = 0x01;
+    } else if (y < (World.y/2)+4) {
+        block = 0x03;
+    } else if (y == (World.y/2)+4) {
+        block = 0x02;
     }
-    for (let x = 0; x < lx; x++) {
-        for (let z = 0; z < lz; z++) {
-            for (let y = 0; y < ly; y++) {
-                let block = 0x00;
-                if (y < ly/2) {
-                    block = 0x01;
-                } else if (y < (ly/2)+4) {
-                    block = 0x03;
-                } else if (y == (ly/2)+4) {
-                    block = 0x02;
-                }
-                buffer[4+(y*lx*lz)+(z*lx)+x] = block;
-            }
-        }
-    }
-    for (let i = 0; i < deltas.length; i++) {
-        buffer[4+(deltas[i].y * lx * lz) + (deltas[i].z * lx) + deltas[i].x] = deltas[i].block;
-    }
-    return buffer;
+    return block;
 }
 
 lto.on('login', async (packet, socket) => {
@@ -50,7 +28,7 @@ lto.on('login', async (packet, socket) => {
     let player = {username: packet.Data.username, Position: spawnPos, socket: socket} as Player;
     World.players.set(socket.data.PlayerID, player);
     // now we send them da LEVELLLL
-    let worldGZ = Bun.gzipSync(await buildWorld(World, deltas));
+    let worldGZ = Bun.gzipSync(await buildWorld(World, getBlock));
     sendWorld(World, worldGZ, socket);
     spawnPlayer(socket, player, World.players)
     broadcast(World.players, await returnChatMsg(packet.Data.username + ' has joined!', socket.data.PlayerID));
@@ -58,9 +36,8 @@ lto.on('login', async (packet, socket) => {
 
 lto.on('block', async (packet, socket, data) => {
     console.log(`${World.players.get(socket.data.PlayerID).username} placed ${packet.Data.block} at (${packet.Data.x},${packet.Data.y},${packet.Data.z})`);
-    let resp = [0x06, data[1], data[2], data[3], data[4], data[5], data[6], packet.Data.block];
-    broadcast(World.players, await parseTypes(resp, ['hex', 'hex', 'hex', 'hex', 'hex', 'hex', 'hex', 'hex']));
-    deltas.push(packet.Data);
+    placeBlock(World, packet);
+    World.deltas.push(packet.Data);
 })
 
 lto.on('pos', async (packet, socket, data) => {
--- utils.ts
+++ utils.ts
@@ -1,4 +1,5 @@
-import { parseTypes } from './types.ts'
+import { parseTypes } from './types.ts';
+import { broadcast } from './socketHelpers.ts';
 export async function returnChatMsg(string, playerID) {
     let pid = 0x0d;
     return parseTypes([pid, playerID, string], ['hex', 'hex', 'string']);
@@ -8,7 +9,7 @@ export async function buildWorld(World, func) {
     let lx = World.x;
     let lz = World.z;
     let ly = World.y;
-   let size = lx * ly * lz;
+    let size = lx * ly * lz;
     let hexarr = size.toString(16).padStart(8, "0").match(/\d{2}/g);
     const buffer = Buffer.alloc(size);
     for (let i = 0; i < hexarr.length; i++) {
@@ -16,24 +17,21 @@ export async function buildWorld(World, func) {
         buffer[i] = hexarr[i];
         //writer.write(buffer);
     }
-    for (let x = 0; x < lx; x++) {
+    for (let y = 0; y < ly; y++) {
         for (let z = 0; z < lz; z++) {
-            for (let y = 0; y < ly; y++) {
-                let block = 0x00;
-                if ((x-128)*(x-128) + (z-128)*(z-128) < 16384) {
-                    if (y % 10 == 0) {
-                    block = 30;
-                    }
-                } else {
-                    block = 33; 
-                }
-                buffer[4+(y*lx*lz)+(z*lx)+x] = block;
+            for (let x = 0; x < lx; x++) {
+                buffer[4+(y*lx*lz)+(z*lx)+x] = func(World, x, y, z);
             }
         }
     }
-    for (let i = 0; i < deltas.length; i++) {
-        buffer[4+(deltas[i].y * lx * lz) + (deltas[i].z * lx) + deltas[i].x] = deltas[i].block;
+    for (let i = 0; i < World.deltas.length; i++) {
+        buffer[4+(World.deltas[i].y * lx * lz) + (World.deltas[i].z * lx) + World.deltas[i].x] = World.deltas[i].block;
     }
     return buffer;
+}
+
+export async function placeBlock(World, packet) {
+    let resp = [0x06, packet.Data.x, packet.Data.y, packet.Data.z, packet.Data.block];
+    broadcast(World.players, await parseTypes(resp, ['hex', 'short', 'short', 'short', 'hex']));
 }
 

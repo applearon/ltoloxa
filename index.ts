04598cc81ed99d000f7a230da26fe5ef3d996c8b
applearon <aronseshan@gmail.com>
2024-11-15 04:24:26 -0500

    Removed Old Comments

--- index.ts
+++ index.ts
@@ -13,15 +13,12 @@ export class Server {
           port: port,
           socket: {
             async data(socket, data) {
-                //console.log("sup", data);
-                //console.log(typeof data);
                 let packet = await parseClientData(socket, data) as ClientPacket;
                 switch (packet.ID) {
                     case 0x00: { // login
                         let Pdata = packet.Data as CPlayerID;
                         if (Pdata.PVersion === 0x07) { // Assumes you want only classic v0.30
                             console.log(`${Pdata.username} attempting to join`);
-                            //lto.emit('login', packet, socket, data);
                             await loginCallback(packet, socket, data);
                         } else {
                             console.log("Invalid Packet Version, closing connection");
@@ -29,15 +26,12 @@ export class Server {
                         }
                     } break;
                     case 0x05: { // place/break block
-                        //lto.emit('block', packet, socket, data);
                         await blockCallback(packet, socket, data);
                     } break;
                     case 0x08: { // movement
-                        //lto.emit('pos', packet, socket, data);
                         await movementCallback(packet, socket, data);
                     } break;
                     case 0x0d: { // chat msg
-                        //lto.emit('chat', packet, socket, data);
                         await chatCallback(packet, socket, data);
                     } break;
                     default: {
@@ -45,23 +39,21 @@ export class Server {
         
                     }
                 }
-            }, // message received from client
+            }, 
             async open(socket) {
                 console.log("opened!");
                 socket.data = {PlayerID: -1} as SocketData;
-                //socket.data = { PlayerID: await getID(players)};
-            }, // socket opened
+            },
             async close(socket) {
                 console.log("We got closed :(");
-                //lto.emit('disconnect', socket);
                 await disconnectCallback(socket);
-            }, // socket closed
+            },
             drain(socket) {
                 console.log("got drained");
-            }, // socket ready for more data
+            },
             error(socket, error) {
                 console.error("AAAA: ", error);
-            }, // error handler
+            },
           },
         });
     }
